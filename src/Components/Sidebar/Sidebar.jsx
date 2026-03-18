4f948269ae7f349f39d335fc768b4656d40797d4
KODIGIT <49615013+kodigit1@users.noreply.github.com>
2026-03-18 15:13:18 +0100

    Update Sidebar.jsx
    delete archive in sidebar

--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -105,18 +105,7 @@ function Sidebar() {
                         <li><a href="404_page">Error 404</a></li>
                     </ul>
                 </li>
-                <li className="sidebar-dropdown">
-                    <div className="dropdown-header">
-                        <a href="#">Archive</a>
-                    <button className="sidebar-dropdown-btn">
-                        <i className="fa-solid fa-angle-down"></i>
-                    </button>
-                    </div>
-                    <ul className="sidebar-dropdown-menu">
-                        <li><a href="blog">Blog</a></li>
-                        <li><a href="single_post">Single Post</a></li>
-                    </ul>
-                </li>
+                
                 <li className="below-dropdown"><a href="contact">Contact Us</a></li>
             </ul>
         </div>
