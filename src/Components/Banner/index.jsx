b6eed1a626822cff004f5ba55efeb0487130cf4f
KODIGIT <49615013+kodigit1@users.noreply.github.com>
2026-03-18 11:08:37 +0100

    Refactor BannerHomeSection component

--- src/Components/Banner/index.jsx
+++ src/Components/Banner/index.jsx
@@ -108,7 +108,7 @@ function BannerHomeSection() {
                         <div className="d-flex flex-column gspace-2">
                             <AnimateOnScroll animation="fadeInLeft" speed="normal">
                                 <h1 className="title-heading-banner">
-                                    Amplify Your Brand with Cutting-Edge Digital Marketing
+                                    Il tuo nuovo sito web a € 9,99/mese
                                 </h1>
                             </AnimateOnScroll>
                             <div className="banner-heading">
