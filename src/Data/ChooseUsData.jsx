54a77cf2a8c994f067dc0441bab0a58bdc01b86a
KODIGIT <sales@kodigit.agency>
2026-03-26 10:10:42 +0100

    Update components

--- src/Data/ChooseUsData.jsx
+++ src/Data/ChooseUsData.jsx
@@ -2,24 +2,24 @@ export const whychooseus = [
     {
         id: 1,
         icon: "/assets/images/Icon-2.png",
-        title: "Data-Driven Approach",
-        content: "Every decision is backed by real-time analytics for maximum impact lorem ipsum dolor sit  consectetur adipiscing elit ut elit tellus luctus nec.",
+        title: "Ho.Re.Ca.",
+        content: "ristoranti, trattorie, pizzerie, bar, pub, caffè, gelaterie, servizi di catering e banqueting.",
         link: "#",
         speed: "fast"
     },
     {
         id: 2,
         icon: "/assets/images/icon-1.png",
-        title: "Creative & Inovative",
-        content: "Cutting-edge marketing techniques to keep you ahead of the competition lorem ipsum dolor sit awsa consectetur adipiscing elit ut elit.",
+        title: "Attività Tradizionali",
+        content: "Attività di quartiere come hair-stylist, barberie, cartolerie cha hanno la necessità di più visibiltà.",
         link: "#",
         speed: "normal"
     },
     {
         id: 3,
         icon: "/assets/images/Icon-3.png",
-        title: "Transparent Reporting",
-        content: "Clear insights and performance tracking, so you always know your ROI lorem ipsum dolo consectetur adipiscing elit ut elit tellus luctus nec.",
+        title: "Artigiani",
+        content: "Idraulici, elettricisti, meccanici auto,imbianchini, che non hanno tempo di dedicarsi ad attività di marketing.",
         link: "#",
         speed: "slow"
     },
